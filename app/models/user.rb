6f0da53b81ef2fd8c8fd92896254d5671347c2f0
straysheep3 <getloose.kage@gmail.com>
2016-06-06 01:12:57 +0900

    paperclipでヘッダーにサムネイルを表示できるようにこれからする

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :rooms
   has_many :chats
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
